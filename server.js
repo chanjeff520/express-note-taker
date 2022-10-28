db799e30e72daa696f43c8aa861a08b47f7dfaee
chanjeff520 <chanjeff520@gmail.com>
2022-10-28 02:59:12 -0400

    add get to api/notes

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+//required packages and modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util')
 
 //the port we are using
 const PORT = process.env.port || 3001;
 //instanciates the express function
 app = express();
 
-//use to access the public folder
+//middleware for parsing JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.use(express.static('public'));
 
 //for the home page of note taking
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
 //for the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+});
+
+app.get('/api/notes', (req, res) => {
+    util.promisify(fs.readFile)('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 //sets up the webpage with a port of 3001 or the port given
 app.listen(PORT, () => {
